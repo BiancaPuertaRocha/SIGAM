3b2f2969fe76c19c33d88dfe6ec231d2f727be57
Abner Silva <abner.js05@gmail.com>
2019-10-01 12:38:22 -0300

    esboço do logo com botoes

--- build/generated-sources/ap-source-output/model/Funcionario_.java
+++ build/generated-sources/ap-source-output/model/Funcionario_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-30T16:59:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-01T10:39:30")
 @StaticMetamodel(Funcionario.class)
 public class Funcionario_ extends Pessoa_ {
 
