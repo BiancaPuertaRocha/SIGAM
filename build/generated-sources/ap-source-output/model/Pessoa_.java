917ba8a79c30b6104b4c3b29d7a57a9ccfbc53ad
Abner Silva <abner.js05@gmail.com>
2019-10-01 15:12:03 -0300

    fixed "S" alignment, added hover color to logo, changed background image change close button properties, added move windows action

--- build/generated-sources/ap-source-output/model/Pessoa_.java
+++ build/generated-sources/ap-source-output/model/Pessoa_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-01T10:39:30")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-01T15:00:16")
 @StaticMetamodel(Pessoa.class)
 public class Pessoa_ { 
 
